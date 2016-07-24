709142c5c800b0108bac449df38d27bfb25c0d21
JustinYang <ityangjing@gmail.com>
2016-07-25 00:56:03 +0800

    new vc

---
+++ QueryInfo/BNRSettingVC.h
@@ -0,0 +1,13 @@
+//
+//  BNRSettingVC.h
+//  QueryInfo
+//
+//  Created by JustinYang on 7/25/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRSettingVC : UIViewController
+
+@end
---
+++ QueryInfo/BNRSettingVC.m
@@ -0,0 +1,29 @@
+//
+//  BNRSettingVC.m
+//  QueryInfo
+//
+//  Created by JustinYang on 7/25/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import "BNRSettingVC.h"
+
+@interface BNRSettingVC ()
+
+@end
+
+@implementation BNRSettingVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
