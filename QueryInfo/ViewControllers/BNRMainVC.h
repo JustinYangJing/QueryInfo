95dc0c195e1862d4602c516e91ddcf1e22a622e3
JustinYang <ityangjing@gmail.com>
2016-07-24 23:29:06 +0800

    new project

---
+++ QueryInfo/ViewControllers/BNRMainVC.h
@@ -0,0 +1,13 @@
+//
+//  BNRMainVC.h
+//  QueryInfo
+//
+//  Created by JustinYang on 7/24/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRMainVC : UIViewController
+
+@end
