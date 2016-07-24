b9702f12cf2faa39185451b1a62e15c239216f3c
JustinYang <ityangjing@gmail.com>
2016-07-25 01:13:47 +0800

    cell

---
+++ QueryInfo/BNRTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BNRTableViewCell.h
+//  QueryInfo
+//
+//  Created by JustinYang on 7/25/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRTableViewCell : UITableViewCell
+
+@end
---
+++ QueryInfo/BNRTableViewCell.m
@@ -0,0 +1,28 @@
+//
+//  BNRTableViewCell.m
+//  QueryInfo
+//
+//  Created by JustinYang on 7/25/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import "BNRTableViewCell.h"
+
+@interface BNRTableViewCell()
+
+@end
+
+@implementation BNRTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
